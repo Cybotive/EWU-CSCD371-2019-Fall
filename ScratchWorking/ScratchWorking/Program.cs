b6258403a6db265fec2c05deacd1e97972016564
Josh Lini <networkmisc@gmail.com>
2019-10-29 14:28:52 -0700

    Exception handling stuff

--- ScratchWorking/ScratchWorking/Program.cs
+++ ScratchWorking/ScratchWorking/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace ScratchWorking
 {
@@ -6,7 +8,20 @@ namespace ScratchWorking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                //IO Stuff
+                Console.WriteLine("Hello World!");
+            }
+            catch (IOException ex) when (ex.Message == "this message") //this when statement reduces callstack load
+            {
+                //Do something
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"{e}"); //this is very bad. Don't log then rethrow without handling the exception
+                throw;
+            }
         }
     }
 }
