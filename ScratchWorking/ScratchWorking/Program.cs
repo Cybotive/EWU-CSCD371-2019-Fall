73486cc0f1d282bf3902384e65d264fc5a4c13ef
Josh Lini <networkmisc@gmail.com>
2019-10-29 14:24:36 -0700

    Created Base Console App

---
+++ ScratchWorking/ScratchWorking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScratchWorking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
